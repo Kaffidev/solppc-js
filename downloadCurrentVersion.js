162ebd837b8717db9c7d5709599091381e8d8bf8
Nihat <kaffinpx@gmail.com>
2021-11-06 14:33:24 +0300

    Should work

--- downloadCurrentVersion.js
+++ downloadCurrentVersion.js
@@ -3,30 +3,11 @@
 // This is used to download the correct binary version
 // as part of the prepublish step.
 
-var pkg = require('./package.json');
 var fs = require('fs');
 var https = require('follow-redirects').https;
-var MemoryStream = require('memorystream');
-var keccak256 = require('js-sha3').keccak256;
 
-function getVersionList (cb) {
-  console.log('Retrieving available version list...');
-
-  var mem = new MemoryStream(null, { readable: false });
-  https.get('https://solc-bin.ethereum.org/bin/list.json', function (response) {
-    if (response.statusCode !== 200) {
-      console.log('Error downloading file: ' + response.statusCode);
-      process.exit(1);
-    }
-    response.pipe(mem);
-    response.on('end', function () {
-      cb(mem.toString());
-    });
-  });
-}
-
-function downloadBinary (outputName, version, expectedHash) {
-  console.log('Downloading version', version);
+function downloadBinary (outputName) {
+  console.log('Downloading latest version...');
 
   // Remove if existing
   if (fs.existsSync(outputName)) {
@@ -40,7 +21,7 @@ function downloadBinary (outputName, version, expectedHash) {
   });
 
   var file = fs.createWriteStream(outputName, { encoding: 'binary' });
-  https.get('https://solc-bin.ethereum.org/bin/' + version, function (response) {
+  https.get('github.com/vitelabs/soliditypp/releases/download/latest/soljson.js', function (response) {
     if (response.statusCode !== 200) {
       console.log('Error downloading file: ' + response.statusCode);
       process.exit(1);
@@ -48,28 +29,12 @@ function downloadBinary (outputName, version, expectedHash) {
     response.pipe(file);
     file.on('finish', function () {
       file.close(function () {
-        var hash = '0x' + keccak256(fs.readFileSync(outputName, { encoding: 'binary' }));
-        if (expectedHash !== hash) {
-          console.log('Hash mismatch: ' + expectedHash + ' vs ' + hash);
-          process.exit(1);
-        }
         console.log('Done.');
       });
     });
   });
 }
 
-console.log('Downloading correct solidity binary...');
+console.log('Downloading correct soliditypp binary...');
 
-getVersionList(function (list) {
-  list = JSON.parse(list);
-  var wanted = pkg.version.match(/^(\d+\.\d+\.\d+)$/)[1];
-  var releaseFileName = list.releases[wanted];
-  var expectedFile = list.builds.filter(function (entry) { return entry.path === releaseFileName; })[0];
-  if (!expectedFile) {
-    console.log('Version list is invalid or corrupted?');
-    process.exit(1);
-  }
-  var expectedHash = expectedFile.keccak256;
-  downloadBinary('soljson.js', releaseFileName, expectedHash);
-});
+downloadBinary('soljson.js');
